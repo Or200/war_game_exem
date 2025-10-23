9634666391e2def9b016f14746828a7fe2616028
Or200 <avimeda74@gmail.com>
2025-10-23 12:31:54 +0300

    finish game

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ from game_logic.game import *
 
 if __name__ == "__main__":
     satrt = init_game()
-    play_round(satrt["player_1"], satrt["player_2"])
+    while satrt["player_1"]["hand"] and satrt["player_2"]["hand"]:
+        play_round(satrt["player_1"], satrt["player_2"])
+        
+    if len(satrt["player_1"]["won_pile"]) > len(satrt["player_2"]["won_pile"]):
+        print(f"Game Winner is:  !!!! {satrt['player_1']["name"]} !!!!")
+    elif len(satrt["player_1"]["won_pile"]) < len(satrt["player_2"]["won_pile"]):
+        print(f"Game Winner is:  !!!! {satrt['player_2']["name"]} !!!!")
+    else:
+        print("Win Win")
